7e8cdb5c9de3b259dbc7172d8a6c3569ac3df609
joeywang310 <wangzhicheng310@126.com>
2019-12-03 17:06:27 +0800

    thread 交替打印0和1

--- thraad/src/com/wzc/thread/produceANDcomsumer/demo2/Print0and1.java
+++ thraad/src/com/wzc/thread/produceANDcomsumer/demo2/Print0and1.java
@@ -1,5 +1,7 @@
 package com.wzc.thread.produceANDcomsumer.demo2;
 
+import java.util.List;
+
 /**
  * @program: java-learn
  * @description: 交替打印0和1,
@@ -7,5 +9,51 @@ package com.wzc.thread.produceANDcomsumer.demo2;
  * @create: 2019-12-03 16:47
  **/
 public class Print0and1 {
+
+    public static final String LOCK = "LOCK";
+    static boolean flag = false;
+
+    public static void main(String[] args) {
+
+
+        new Thread(() -> {
+            synchronized (LOCK) {
+                while (true) {
+                    if (flag) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println(0);
+                        flag = true;
+                        LOCK.notify();
+                    }
+                }
+            }
+
+        }).start();
+
+        new Thread(() -> {
+            synchronized (LOCK) {
+                while (true) {
+                    if (!flag) {
+                        try {
+                            LOCK.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        System.out.println(1);
+                        flag = false;
+                        LOCK.notify();
+                    }
+                }
+            }
+
+
+        }).start();
+    }
 
 }
